bb6d00f7dc7519c9652f1a6a087478ca68096f9e
David MARIE <eth3rnit3@outlook.fr>
2017-09-01 17:40:23 +0200

    add login form or walt app if you are loggin, work in local

--- js/back.js
+++ js/back.js
@@ -1,10 +1,11 @@
 $( document ).ready(function() {
-
-  if (localStorage.getItem("user") != null){
-    $( "#login" ).hide();
+  if (localStorage.getItem("user") == null){
+    $( "#logged" ).hide();
+    $( "#unlogged" ).show();
   }
   else{
-    $( "#login" ).show();
+    $( "#logged" ).show();
+    $( "#unlogged" ).hide();
   };
 
 });
--- js/connect/log_in.js
+++ js/connect/log_in.js
@@ -1,17 +1,19 @@
 $( document ).ready(function() {
+  // variable contenant les headers ainsi que l'url de connection a l'api
   var ajaxHeaders = {
     "accept": "application/json",
   };
 
   var apiBaseUrl = "http://localhost:3000";
-
+  // on pose un micro sur le formulaire qui ecoute lorsque l'on clique sur submit
   $( "#new_user" ).on('submit', function(event) {
+    // echape l'evenement par defaut du bouton submit
     event.preventDefault();
-
+    // recupère le contenu des inputs
     var email = $("#user_email").val();
     var password = $("#user_password").val();
 
-    // send at ajax and try to login
+    // envoi les données à la fonction ajaxLogin pour tenter de se connecter
     ajaxLogin(email, password);
   });
 
@@ -30,18 +32,20 @@ $( document ).ready(function() {
       data: data,
       dataType: "json",
       success: function(data) {
+        // en cas de success
         var user = {
           email: data.email,
           token: data.authentication_token
         };
-
+        // on sauvegarde en local l'email et le token d'authentification
         localStorage.setItem("user", JSON.stringify(user));
-
-        window.location.href = "./index.html";
+        $( "#logged" ).show();
+        $( "#unlogged" ).hide();
       },
 
       error: function(jqXHR, status, text) {
         if (jqXHR.responseJSON.error) {
+          // en cas d'ereur on affiche une notif contenant le message d'erreur
           $("#form_error")
             .show()
             .html(jqXHR.responseJSON.error);
