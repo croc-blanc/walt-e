488ce35af3fbd24281c28db4d7432b5333abc222
David MARIE <eth3rnit3@outlook.fr>
2017-09-04 13:45:04 +0200

    review merge problem and solve it 2

--- js/back.js
+++ js/back.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
     if (localStorage.getItem("user") != null) {
+        $("#style1").attr("disabled", "disabled");
         $("#logged").show();
         $("#unlogged").hide();
         $('body').css(["width: 500px", "height: 50px;"])
     } else {
         $("#logged").hide();
         $("#unlogged").show();
-        $('head:first').append('<link rel="stylesheet" type="text/css" href="css/loginstyle.css" />');
+        // $('head:first').append('');
         $('body').css(["width: 300px", "height: 200px;"])
 
     };
