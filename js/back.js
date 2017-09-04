42587e4fcb42ff5a81e0349094ce53cb13cdf4e9
David MARIE <eth3rnit3@outlook.fr>
2017-09-04 13:35:58 +0200

    review merge problem and solve it

--- js/back.js
+++ js/back.js
@@ -1,16 +1,14 @@
-$( document ).ready(function() {
-  if (localStorage.getItem("user") != null){
-    $( "#logged" ).show();
-    $( "#unlogged" ).hide();
-    $('body').css(["width: 500px", "height: 50px;"])
-  }
-  else{
-    $( "#logged" ).hide();
-    $( "#unlogged" ).show();
-    $('body').css(["width: 300px", "height: 200px;"])
-  };
+$(document).ready(function() {
+    if (localStorage.getItem("user") != null) {
+        $("#logged").show();
+        $("#unlogged").hide();
+        $('body').css(["width: 500px", "height: 50px;"])
+    } else {
+        $("#logged").hide();
+        $("#unlogged").show();
+        $('head:first').append('<link rel="stylesheet" type="text/css" href="css/loginstyle.css" />');
+        $('body').css(["width: 300px", "height: 200px;"])
+
+    };
 
 });
-
-
-
