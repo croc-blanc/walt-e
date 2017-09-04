5eba40e211e5243961f2d870bec23302235a2bec
David MARIE <eth3rnit3@outlook.fr>
2017-09-04 16:07:29 +0200

    add internet url

--- js/connect/sign_up.js
+++ js/connect/sign_up.js
@@ -4,7 +4,8 @@ $(document).ready(function() {
         "accept": "application/json",
     };
 
-    var apiBaseUrl = "http://localhost:3000";
+    // var apiBaseUrl = "http://127.0.0.1:3000";
+    var apiBaseUrl = "https://walt-ia.herokuapp.com";
     // on pose un micro sur le formulaire qui ecoute lorsque l'on clique sur submit
     $("#new_user").on('submit', function(event) {
         // echape l'evenement par defaut du bouton submit
@@ -43,8 +44,7 @@ $(document).ready(function() {
                 localStorage.setItem("user", JSON.stringify(user));
                 $("#logged").show();
                 $("#unlogged").hide();
-                window.location.href = 'index.html';
-                location.reload();
+
             },
 
             error: function(jqXHR, status, text) {
--- js/notify.js
+++ js/notify.js
@@ -1,151 +1,159 @@
 $(document).ready(function() {
-  // si l'user est logger, on recupère le contenu du localstorage 'user' et recrer un objet
-  if (localStorage.getItem("user") != null){
-    var ajaxHeaders = {
-                        "X-User-Email": JSON.parse(localStorage.getItem("user")).email,
-                        "X-User-Token": JSON.parse(localStorage.getItem("user")).token
-                      };
-      // Appelle la fonction qui ce connecte à l'api en ajax au chargement de la page puis toute les 2 min
-      backRequest();
-      setInterval(function() {
+    var remindersIntervalId;
+
+    // si l'user est logger, on recupère le contenu du localstorage 'user' et recrer un objet
+    if (localStorage.getItem("user") != null) {
+        var ajaxHeaders = {
+            "X-User-Email": JSON.parse(localStorage.getItem("user")).email,
+            "X-User-Token": JSON.parse(localStorage.getItem("user")).token
+        };
+        // Appelle la fonction qui ce connecte à l'api en ajax au chargement de la page puis toute les 2 min
         backRequest();
-      }, 60 * 1000);
+        setInterval(function() {
+            backRequest();
+        }, 10 * 1000);
     };
 
 
+    // var apiBaseUrl = "http://127.0.0.1:3000";
+    var apiBaseUrl = "https://walt-ia.herokuapp.com";
 
-  var apiBaseUrl = "http://127.0.0.1:3000/api/v1";
-
-  // check si les notifs sont disponible sur le navigateur
-  if (!Notification) {
-    alert('Les notifications ne sont pas disponible sur votre navigateur. Essayer Chromium.');
-    return;
-  }
-  // si les notif sont desactive, demande l'autorisation de les activées
-  if (Notification.permission !== "granted") {
-    Notification.requestPermission();
-  }
-
-
-
-  function backRequest(){
-    $.ajax({
-      // hender de connexion a l'api pour recevoir la liste des reminders
-      type: "GET",
-      url: apiBaseUrl + "/reminders",
-      headers: ajaxHeaders,
-      success: function(data) {
-        // appelle la fonction qui traitera les datas
-        handleReminders(data);
-      },
-      error: function(jqXHR) {
-        console.log(jqXHR.responseText);
-      }
-    });
-  }
-
-
-  function handleReminders(reminders) {
-    var newReminders = []
-    // ajoute les reminders seulement si il ne sont pas encore passé
-    reminders.forEach(function(reminder) {
-      if (Date.parse(reminder.time) > Date.now()) {
-        newReminders.push(reminder)
-      }
-    });
-    // transforme l'array de reminders (JSON) en string
-    var stringify = JSON.stringify(newReminders);
-    console.log(stringify);
-    // appelle la fonction qui sauvegarde les données en local
-    storeReminder(stringify);
-
-  };
-
-
-  function storeReminder(reminder) {
-    // stocke chaque reminder en local storage dans une "variable" appelé "remindplusiduser"
-    localStorage.setItem("remindplusiduser", reminder);
-    // recupere les données locales (json stingify)
-    getStoredReminders();
-  };
-
-  function getStoredReminders() {
-    // recupere la stringify des reminders
-    var remindString = localStorage.getItem("remindplusiduser");
-    // appelle la fonction pour reconstruire le local storage en JSON
-    parseToJson(remindString);
-  };
-
-  function parseToJson(string){
-    // transforme la string du local storage en JSON
-    var remindJson = JSON.parse(string);
-    // envoie le JSON a la fonction qui check l'heure actuelle et l'heure du reminder
-    runRemindersTime(remindJson);
-  };
-
-
-  function runRemindersTime(reminders) {
-    setInterval(function() {
-      reminders.forEach(function(reminder, index) {
-        // comparaison entre l'heure du reminder et l'heure actuelle
-        if (Date.parse(reminder.time) <= Date.now() + 15000) {
-          // affiche une notification si c'est l'heure
-          notifyMe(reminder);
-          reminders.splice(index, 1);
-        }
-      });
-    }, 5 * 1000);
-  }
-  // fonction de notification
-  function notifyMe(reminder) {
+    // check si les notifs sont disponible sur le navigateur
+    if (!Notification) {
+        alert('Les notifications ne sont pas disponible sur votre navigateur. Essayer Chromium.');
+        return;
+    }
+    // si les notif sont desactive, demande l'autorisation de les activées
     if (Notification.permission !== "granted") {
-      Notification.requestPermission();
-    } else {
-      var notification = new Notification('Walt Notification', {
-        icon: '/img/walt_128.png',
-        body: reminder.content,
-        sound: 'sound/walt.wav',
-      });
+        Notification.requestPermission();
+    }
 
-      notification.onclick = function () {
-        //si on clique sur la notif, on appelle la fonction snooze
-        snoozeNotif(reminder);
+
+
+    function backRequest() {
+        $.ajax({
+            // hender de connexion a l'api pour recevoir la liste des reminders
+            type: "GET",
+            url: apiBaseUrl + "/reminders",
+            headers: ajaxHeaders,
+            success: function(data) {
+                // appelle la fonction qui traitera les datas
+                if (data != []) {
+                    handleReminders(data);
+                };
+            },
+            error: function(jqXHR) {
+                console.log(jqXHR.responseText);
+            }
+        });
+    }
+
+
+    function handleReminders(reminders) {
+        var newReminders = []
+            // ajoute les reminders seulement si il ne sont pas encore passé
+        reminders.forEach(function(reminder) {
+            if (Date.parse(reminder.time) > Date.now()) {
+                newReminders.push(reminder)
+            }
+        });
+        // transforme l'array de reminders (JSON) en string
+        var stringify = JSON.stringify(newReminders);
+        // appelle la fonction qui sauvegarde les données en local
+        storeReminder(stringify);
+
+    };
+
+
+    function storeReminder(reminder) {
+        // stocke tous reminder en local storage dans une "variable" appelé "remindplusiduser"
+        localStorage.setItem("remindplusiduser", reminder);
+        // recupere les données locales (json stingify)
+        getStoredReminders();
+    };
+
+    function getStoredReminders() {
+        // recupere la stringify des reminders
+        var remindString = localStorage.getItem("remindplusiduser");
+        // appelle la fonction pour reconstruire le local storage en JSON
+        parseToJson(remindString);
+    };
+
+    function parseToJson(string) {
+        // transforme la string du local storage en JSON
+        var remindJson = JSON.parse(string);
+        // envoie le JSON a la fonction qui check l'heure actuelle et l'heure du reminder
+        console.log(remindJson);
+        runRemindersTime(remindJson);
+    };
+
+
+    function runRemindersTime(reminders) {
+      if (remindersIntervalId) {
+        clearTimeout(remindersIntervalId);
       };
+
+      remindersIntervalId = setInterval(function() {
+            reminders.forEach(function(reminder, index) {
+                // comparaison entre l'heure du reminder et l'heure actuelle
+                if (Date.parse(reminder.time) <= Date.now() + 15000) {
+                    // affiche une notification si c'est l'heure
+                    notifyMe(reminder);
+                    reminders.splice(index, 1);
+                }
+            });
+        }, 5 * 1000);
+    }
+    // fonction de notification
+    function notifyMe(reminder) {
+        if (Notification.permission !== "granted") {
+            Notification.requestPermission();
+        } else {
+            var notification = new Notification('Walt Notification', {
+                icon: '/img/walt_128.png',
+                body: reminder.content,
+                sound: 'sound/walt.wav',
+            });
+
+            notification.onclick = function() {
+                //si on clique sur la notif, on appelle la fonction snooze
+                snoozeNotif(reminder);
+            };
+
+        }
 
     }
 
-  }
 
-
-  function snoozeNotif(reminder) {
-    // transforme les attributs de du reminder a répéter avant de le transmettre au serveur
-    var newTime = Date.parse(reminder.time) + 30000; // snooze pour 30 sec
-    reminder.jstime = newTime;
-    reminder.id = null;
-    reminder.time = null;
-    var action = {
-      type: "reminders",
-      when: newTime,
-      content: reminder.content
+    function snoozeNotif(reminder) {
+        // transforme les attributs de du reminder a répéter avant de le transmettre au serveur
+        var newTime = Date.parse(reminder.time) + 30000; // snooze pour 30 sec
+        reminder.jstime = newTime;
+        reminder.id = null;
+        reminder.time = null;
+        var action = {
+            type: "reminders",
+            when: newTime,
+            content: reminder.content
+        };
+        var hash = {
+            reminder: action
+        };
+        $.ajax({
+            type: "POST",
+            url: apiBaseUrl + "/api/v1/reminders",
+            headers: ajaxHeaders,
+            data: hash,
+            success: function(data) {
+                console.log("SNOOZE Success: " + data);
+                alert("La répétition dans 5 min à était activé");
+            },
+            error: function(jqXHR) {
+                console.error(jqXHR.responseText);
+                alert("Une erreur est survenue :" + jqXHR.responseText)
+                console.dir(hash);
+            }
+        });
     };
-    var hash = {
-      reminder: action
-    };
-    $.ajax({
-      type: "POST",
-      url: apiBaseUrl + "/reminders",
-      headers: ajaxHeaders,
-      data: hash,
-      success: function(data) {
-        console.log("SNOOZE Success: " + data);
-        alert("La répétition dans 5 min à était activé");
-      },
-      error: function(jqXHR) {
-        console.error(jqXHR.responseText);
-        alert("Une erreur est survenue :" + jqXHR.responseText)
-        console.dir(hash);
-      }
-    });
-  };
 
 });
